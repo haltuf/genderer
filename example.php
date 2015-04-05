b760b5bef169a0fcd32c5e5c5af0ca5b04e21781
haltuf <haltuf@imagineo.cz>
2015-04-05 18:31:30 +0200

    typo

--- example.php
+++ example.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . '/vendor/autoload.php' ;
 
 $g = new Haltuf\Genderer\Genderer ;
-echo "Dobrý den, " . $g->getVocative("Tomáš Vomáčka");
+echo "Dobrý den, " . $g->getVocative( "Tomáš Vomáčka" );
 
 // 'm' = male, 'f' = female
-echo "Pohlaví: " . $g->getGender("Tomáš Vomáčka");
+echo "Pohlaví: " . $g->getGender( "Tomáš Vomáčka" );
