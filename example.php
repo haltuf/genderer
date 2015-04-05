7bacbc8ad422c8288598b9a41a53fdf8d3f218fe
haltuf <haltuf@imagineo.cz>
2015-04-05 18:07:17 +0200

    example
    example extended with gender detection use case

--- example.php
+++ example.php
@@ -4,3 +4,6 @@ require_once __DIR__ . '/vendor/autoload.php' ;
 
 $g = new Haltuf\Genderer\Genderer ;
 echo "Dobrý den, " . $g->getVocative("Tomáš Vomáčka");
+
+// 'm' = male, 'f' = female
+echo "Pohlaví: " . $g->getGender("Tomáš Vomáčka");
