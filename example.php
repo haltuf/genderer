7d6e79868766d1e0c94be3ebdcd6a5b0b58df597
haltuf <haltuf@imagineo.cz>
2015-04-15 11:13:08 +0200

    updated example, readme.md

--- example.php
+++ example.php
@@ -1,9 +1,29 @@
 <?php
 
+use Haltuf\Genderer\Genderer ;
+
 require_once __DIR__ . '/vendor/autoload.php' ;
 
-$g = new Haltuf\Genderer\Genderer ;
+/**
+ * This part is usually done automatically by Nette Framework.
+ * Just register extension and use @inject annotation.
+ */
+	$connection = new \Nette\Database\Connection( 'sqlite:' . __DIR__ . '/src/Genderer/names.db3' );
+	$cacheStorage = new \Nette\Caching\Storages\FileStorage( __DIR__ );
+	$structure = new \Nette\Database\Structure( $connection, $cacheStorage );
+	$context = new \Nette\Database\Context($connection,$structure);
+	$driver = new \Haltuf\Genderer\Connection($context);
+	$g = new Genderer($driver) ;
+/**
+ * END OF PART THAT WON'T BE USUALLY IN YOUR SCRIPTS
+ */
+
+// Dobrý den, Tomáši Vomáčko
 echo "Dobrý den, " . $g->getVocative( "Tomáš Vomáčka" );
 
 // 'm' = male, 'f' = female
 echo "Pohlaví: " . $g->getGender( "Tomáš Vomáčka" );
+
+// Works for names with more than two parts
+// Dobrý den, MUDr. Tomáši Amosi Březino
+echo "Dobrý den, " . $g->getVocative( "MUDr. Tomáš Amos Březina" ) ;
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-﻿Genderer
+Genderer
 ========
 
 [![Build Status](https://travis-ci.org/haltuf/genderer.svg?branch=master)](https://travis-ci.org/haltuf/genderer)[![Coverage Status](https://coveralls.io/repos/haltuf/genderer/badge.svg)](https://coveralls.io/r/haltuf/genderer)
@@ -8,7 +8,32 @@ Knihovna pro detekci pohlaví podle jména. Kromě toho je schopna dle zadaného
 
 See `example.php` for example of use.
 
-Please be patient, this is my first github project and I use it as a learning playground.
+Please be patient, this is my first github project and I use it as a testing playground.
+
+Installation
+------------
+
+Easiest way to install is to add this line to your `composer.json` file:
+```
+{
+	"require": {
+		"haltuf/genderer": "dev-master"
+	}
+}
+```
+
+or 
+
+```
+composer require haltuf/genderer:@dev
+```
+
+Then it is neccessary to register extension in config.neon:
+
+```
+extensions:
+	genderer: Haltuf\Genderer\Bridges\NetteDI\GendererExtension
+```
 
 Data source
 -----------
